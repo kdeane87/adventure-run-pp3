2372460154321fb35f0efabec7a7cde1f57a2c47
kdeane87 <kdeane87@gmail.com>
2023-06-12 17:46:18 +0000

    Add get_username function

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_username():
+    """
+    Gets the players name
+    """
+    print('Hello')
+    username = input('Enter username:')
+    print('Welcome', username, 'lets get started')
+
+
+get_username()
