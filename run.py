61ad45ad82967ce8aea75ab1bfd8337f6e43cee0
kdeane87 <kdeane87@gmail.com>
2023-06-13 11:57:47 +0000

    add forest_game function

--- run.py
+++ run.py
@@ -13,5 +13,21 @@ def get_username():
             break
 
 
-
 get_username()
+
+def forest_game():
+    """
+    Runs the forest themed game
+    """
+
+    answer = input('You find yourself at the edge of a dense forest. The entrance looks dark and mysterious.\nDo you:\na)Enter the forest\nb)Walk away and find a new adventure\n')
+    if answer == 'a':
+        answer = input('You enter the forest and walk along the winding path.\n The trees are tall and the leaves rustle in the wind.\n Suddenly you hear a loud grown.\nDo you:\na)Run away\nb)Investigate the source of the noise')
+    else:
+        print('You left the game\n')
+    if answer == 'b':
+        print('You follow the sound and find a wounded wolf. It looks at you with pleading eyes.\n Do you:\n a)Help the wolf \n b) Leave the wolf and continus on your journey')
+    else:
+        print('You ran away. Game over!!!')           
+
+forest_game()    
