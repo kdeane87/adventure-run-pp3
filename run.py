927be7b8fa924a7183dc71505cabb4b54ea37d0b
kdeane87 <kdeane87@gmail.com>
2023-06-13 18:17:47 +0000

    add haunted house function

--- run.py
+++ run.py
@@ -15,19 +15,53 @@ def get_username():
 
 get_username()
 
+
+def play_again():
+    """
+    This is called after game is over and gives choice to play again
+    """
+    answer = input('Do you want to play again? \n yes or no \n')
+    if answer == 'yes':
+        print('Ok, lets play again')
+    elif answer == 'no':
+        print('Goodbye')
+
+
+
+#bplay_again()
+
+
 def forest_game():
     """
     Runs the forest themed game
     """
 
-    answer = input('You find yourself at the edge of a dense forest. The entrance looks dark and mysterious.\nDo you:\na)Enter the forest\nb)Walk away and find a new adventure\n')
+    answer = input('You find yourself at the edge of a dense forest. The entrance looks dark and mysterious.\n Do you:\n a)Enter the forest\n b)Walk away and find a new adventure\n')
     if answer == 'a':
-        answer = input('You enter the forest and walk along the winding path.\n The trees are tall and the leaves rustle in the wind.\n Suddenly you hear a loud grown.\nDo you:\na)Run away\nb)Investigate the source of the noise')
+        answer = input('You enter the forest and walk along the winding path.\n The trees are tall and the leaves rustle in the wind.\n Suddenly you hear a loud growl.\nDo you:\n a)Run away\n b)Investigate the source of the noise\n')
     else:
         print('You left the game\n')
+        #run random game function when created
+        exit(play_again())
+        
     if answer == 'b':
         print('You follow the sound and find a wounded wolf. It looks at you with pleading eyes.\n Do you:\n a)Help the wolf \n b) Leave the wolf and continus on your journey')
     else:
-        print('You ran away. Game over!!!')           
+        print('You ran away. Game over!!!')
+        exit(play_again())
+
+    if answer == 'a':
+        print('You help the wolf and bandage its wounds. The wolf is greatful and leads you to a hidden clearing. In the center of the clearing, you see a chest.\n Do you: \n a)Open the chest\n b) Leave the chest and continueon your adventure')
+    else:
+        print('You leave the wolf behind and keep following the path ahead. As you continue you SUDDENLY fall through a trap door and plummet to your death!!!!\n')
+        exit(play_again())
+
 
 forest_game()    
+
+
+def haunted_house_game():
+    """
+    Runs the haunted house game
+    """
+    
