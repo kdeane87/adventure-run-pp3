fcae5eaab79fe3c4f08b6413659825bff66f751d
kdeane87 <kdeane87@gmail.com>
2023-06-13 10:20:52 +0000

    Insert while loop into get_username

--- run.py
+++ run.py
@@ -3,8 +3,15 @@ def get_username():
     Gets the players name
     """
     print('Hello')
-    username = input('Enter username:')
-    print('Welcome', username, 'lets get started')
+    while True:
+        username = input('Enter username:')
+        if username == '':
+            print('You must enter a username')
+            continue    
+        else:        
+            print('Welcome', username, 'lets get started')
+            break
+
 
 
 get_username()
